d089dd3cd36788b2b0e07005a557f691fdf6464f
mfida8526 <mfida8526@365.elmhurst.edu>
2025-11-22 23:45:59 -0600

    added shotgun abilities

--- CGE401-Project5/Assets/Scripts/Shotgun.cs
+++ CGE401-Project5/Assets/Scripts/Shotgun.cs
@@ -7,10 +7,70 @@ using UnityEngine;
     * Project 5
     * Shotgun weapon extension
 */
-public class Shotgun : Weapons
+public class Shotgun : MonoBehaviour, IWeapon
 {
-    private void Start()
+    [Header("Shotgun Settings")]
+    public int pellets = 8;              // Number of pellets per shot
+    public float spreadAngle = 15f;      // Spread angle in degrees
+    public float damage = 8f;            // Damage per pellet
+    public float range = 20f;            // Short-range
+
+    private PlayerShooting shooter;
+
+    void Awake()
     {
-        weaponName = "Shotgun";
+        shooter = GetComponentInParent<PlayerShooting>();
+        if (shooter == null)
+            Debug.LogError("ShotgunWeapon: PlayerShooting not found in parent!");
     }
+
+    public float Damage => damage;
+    public float Range => range;
+
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Shoot()
+    {
+        for (int i = 0; i < pellets; i++)
+        {
+            // Calculate random spread
+            Vector3 direction = shooter.transform.forward;
+            direction = Quaternion.Euler(
+                Random.Range(-spreadAngle, spreadAngle),
+                Random.Range(-spreadAngle, spreadAngle),
+                0
+            ) * direction;
+
+            // Fire the pellet
+            RaycastHit hit;
+            if (Physics.Raycast(shooter.transform.position, direction, out hit, range))
+            {
+                Target target = hit.transform.GetComponent<Target>();
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+
+                    // Notify GameManager
+                    if (target.isInfected)
+                        shooter.GetComponent<GameManager>()?.HitInfectedAnimal();
+                    else
+                        shooter.GetComponent<GameManager>()?.HitNormalAnimal();
+                }
+            }
+        }
+
+        Debug.Log("Shotgun fired!");
+    }
+    /*   private void Start()
+       {
+           weaponName = "Shotgun";
+       }*/
 }
